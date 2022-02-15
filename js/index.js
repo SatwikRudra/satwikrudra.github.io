1a35be838da89c61c975806bf0f03440a2077bec
Satwik Rudra <rudrasatwik19@gmail.com>
2022-02-14 18:06:38 -0600

    changes

--- js/index.js
+++
@@ -1,8 +0,0 @@
-document.body.onload = function(){ /*The onload event occurs when an object has been loaded*/
-  setTimeout(function(){
-    var preloader = document.getElementById('page-preloader');
-    {
-      preloader.classList.add('done');
-    }
-  }, 10000);
-}
